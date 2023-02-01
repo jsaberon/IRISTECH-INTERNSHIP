9684b026c15ae8f7fb9e13c4800a0c28b21d8d35
unknown <ericksoncabillo2@gmail.com>
2023-02-01 10:59:21 +0800

    Addition using JavaScript

---
+++ WK1/JavaScript/calcu.vue
@@ -0,0 +1,25 @@
+
+    <template>
+        <div>
+          <input type="text" v-model="num1"> + 
+          <input type="text" v-model="num2"> = 
+          {{result}}
+        </div>
+      </template>
+      
+      <script>
+      export default {
+        data() {
+          return {
+            num1: 0,
+            num2: 0,
+          };
+        },
+        computed: {
+          result() {
+            return Number(this.num1) + Number(this.num2);
+          },
+        },
+      };
+      </script>
+      
