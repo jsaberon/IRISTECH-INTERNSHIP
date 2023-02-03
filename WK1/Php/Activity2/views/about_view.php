094a6afa9abb361071c5322338ef869513b97975
unknown <ericksoncabillo2@gmail.com>
2023-02-03 12:50:31 +0800

    Php

---
+++ WK1/Php/Activity2/views/about_view.php
@@ -0,0 +1,36 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+
+
+    <h1 class="text-3xl font-bold text-center text-gray-700 py-10">About Us</h1>
+        
+        <p class="text-lg text-gray-700 text-center my-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+            euismod odio, gravida pellentesque urna varius vitae.
+        </p>
+
+        <div class="flex flex-col md:flex-row justify-center items-center">
+            <div class="w-full md:w-1/3 text-center my-5">
+                <img src="https://via.placeholder.com/150" alt="Placeholder Image" class="rounded-full h-64 w-64"/>
+
+                <h2 class="text-lg font-bold text-gray-700">Lorem, ipsum.</h2>
+                <p class="text-gray-700">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+                    euismod odio, gravida pellentesque urna varius vitae.
+                </p>
+            </div>
+
+         <div class="w-full md:w-1/3 text-center my-5">
+        
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image" class="rounded-full h-64 w-64"/>
+            <h2 class="text-lg font-bold text-gray-700">Lorem, ipsum. </h2>
+        
+            <p class="text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+            euismod odio, gravida pellentesque urna varius vitae.
+            </p>
+        </div>
+    
+  
+  
+<?php require('partials/footer.php') ?>
