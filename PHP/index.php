1ce21ebbde9315ab6835206827404fef40585306
japet <jsaberoniris@gmail.com>
2023-02-06 11:12:22 +0800

    PHP demo 3rd activity

--- PHP/index.php
+++ PHP/index.php
@@ -1,4 +1,15 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+
+// Connect to the MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=my_hotel;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from properties where id = 1");
+$statement->execute();
+
+$post = $statement->fetch(PDO::FETCH_ASSOC);
+
+echo "<li>" . $post['name'] . "</li>";
